d0d882ae4ac5eff55a3a385f018f985b9254ec21
Dirk-Jan Wassink <dirk.jan.wassink@gmail.com>
2015-11-02 16:53:34 +0100

    Update node.sh

--- parts/node.sh
+++ parts/node.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-
-curl --silent --location https://deb.nodesource.com/setup_4.x | sudo bash -
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 sudo npm install bower -g
