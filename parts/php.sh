57368c53e993f4518aff178f1b12495fced1ae1c
Dirk-Jan Wassink <dirk.jan.wassink@gmail.com>
2016-01-27 14:40:45 +0100

    upgraded to php 7

--- parts/php.sh
+++ parts/php.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 
-sudo apt-get install -y php5 
+sudo add-apt-repository -y ppa:ondrej/php-7.0 
+sudo apt-get update
+sudo apt-get install -y php7.0
 
-sudo apt-get install -y php5-gd
-sudo apt-get install -y php5-mcrypt
-sudo apt-get install -y php5-curl
-sudo apt-get install -y php5-memcached
-sudo apt-get install -y php5-intl
-sudo apt-get install -y php5-mysql
-sudo apt-get install -y php5-xdebug
-sudo apt-get install -y php5-imagick
-sudo apt-get install -y php5-apcu
+
+sudo apt-get install -y php7.0-gd
+sudo apt-get install -y php7.0-mcrypt
+sudo apt-get install -y php7.0-curl
+sudo apt-get install -y php7.0-intl
+sudo apt-get install -y php7.0-mysql
+
+# https://gist.github.com/nivv/5d9a12af5472b91606e6
+sudo apt-get install -y pkg-config libmagickwand-dev imagemagick build-essential
 
 sudo sh -c "echo 'xdebug.max_nesting_level=500' >> /etc/php5/cli/conf.d/20-xdebug.ini"
 
 sudo php5enmod mcrypt
 
-curl -sS https://getcomposer.org/installer | php
 sudo mkdir /opt/composer
-sudo mv composer.phar /opt/composer/composer
-sudo sh -c "echo 'export PATH=/opt/composer:$PATH' > /etc/environment"
+sudo curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/opt/composer/
+sudo mv /opt/composer/composer.phar /opt/composer/composer
+echo 'export PATH=/opt/composer:$PATH' >> ~/.env
